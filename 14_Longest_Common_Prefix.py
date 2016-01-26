54f71e5426f56aaaacfaff0c5bf053d17c8093db
ChuanLeiGuo <chuanleiguo@gmail.com>
2016-01-26 18:13:27 +0800

    完成了第13 和 14题

---
+++ 14_Longest_Common_Prefix.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        minLen = min(map(len, strs)) if len(strs) != 0 else 0
+        length = 0
+        for i in range(minLen):
+            char = strs[0][i]
+            for j in range(0, len(strs)):
+                if char != strs[j][i]:
+                    return strs[0][: i]
+            length += 1
+        return strs[0][0: length] if len(strs) != 0 else ''
+
+s = ["aca","cba"]
+print Solution().longestCommonPrefix(s)
