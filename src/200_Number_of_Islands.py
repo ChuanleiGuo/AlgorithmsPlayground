b72443cb4466e35fddcf28616578c5c087ef2451
ChuanLeiGuo <chuanleiguo@gmail.com>
2016-07-31 19:15:58 +0800

    Completed 200: Number of Islands --- used Union and Find

--- src/200_Number_of_Islands.py
+++ src/200_Number_of_Islands.py
@@ -1,27 +1,59 @@
+class UF:
+    def __init__(self, n):
+        self.__list = [0] * n
+        for i in range(n):
+            self.__list[i] = i
+
+    def __root(self, i):
+        while i != self.__list[i]:
+            self.__list[i] = self.__list[self.__list[i]]
+            i = self.__list[i]
+        return i
+
+    def connected(self, i, j):
+        return self.__root(i) == self.__root(j)
+
+    def union(self, i, j):
+        ri = self.__root(i)
+        rj = self.__root(j)
+        self.__list[rj] = ri
+
+    def max_union(self, grid):
+        s = set()
+        for i in range(len(self.__list)):
+            x = i / len(grid[0])
+            y = i % len(grid[0])
+            if grid[x][y] == '1':
+                s.add(self.__root(i))
+        return len(s)
+
+
 class Solution(object):
-    dx = [-1, 0, 0, 1]
-    dy = [0, 1, -1, 0]
 
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
-        island = 0
-
         if grid is None or len(grid) == 0:
             return 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    self.__explore(grid, i, j)
-                    island += 1
-        return island
+        height = len(grid)
+        width = len(grid[0])
+        total_size = height * width
+        uf = UF(total_size)
+        for i in range(total_size):
+            x = i / width
+            y = i % width
+            up = x - 1
+            right = y + 1
+            if up >= 0 and grid[x][y] == '1' and grid[x][y] == grid[up][y]:
+                uf.union(i, i - width)
+            if right < width and grid[x][y] == '1' and grid[x][y] == grid[x][right]:
+                uf.union(i, i + 1)
+        return uf.max_union(grid)
 
-    def __explore(self, grid, i, j):
-        grid[i][j] = '0'
-        for d in range(len(self.dx)):
-            if 0 <= i + self.dy[d] < len(grid) and\
-                                    0 <= j + self.dx[d] < len(grid[0]) and \
-                                    grid[i + self.dy[d]][j + self.dx[d]] == '1':
-                self.__explore(grid, i + self.dy[d], j + self.dx[d])
+print Solution().numIslands([
+    '11110',
+    '11010',
+    '11000',
+    '00000'])
