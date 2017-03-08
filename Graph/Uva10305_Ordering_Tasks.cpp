d83d21878ffec1aad203bf2cd8091c7b0f54fa1c
ChuanLeiGuo <chuanleiguo@gmail.com>
2017-03-08 20:25:05 +0800

    completed Uva10305

---
+++ Graph/Uva10305_Ordering_Tasks.cpp
@@ -0,0 +1,48 @@
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+
+using namespace std;
+
+const int maxn = 100;
+
+int g[maxn + 10][maxn + 10], vis[maxn + 1], stack[maxn + 1];
+int pos = 0;
+int u, v, m, n;
+
+int push(int x) {
+    stack[pos ++] = x;
+}
+
+int pop() {
+    return stack[-- pos];
+}
+
+int dfs(int u) {
+    vis[u] = -1;
+    for (int i = 1; i <= n; i ++) {
+        if (g[u][i] && !vis[i]) {
+            dfs(i);
+        }
+    }
+    vis[u] = 1;
+    push(u);
+}
+
+int main() {
+    while (scanf("%d%d", &n, &m) == 2 && (m || n)) {
+        while (m --) {
+            scanf("%d%d", &u, &v);
+            g[u][v] = 1;
+        }
+        memset(vis, 0, sizeof(vis));
+        for (int i = 1; i <= n; i ++) {
+            if (!vis[i]) { dfs(i); } 
+        }
+        while (pos) {
+            printf("%d", pop());
+            printf("%c", pop > 0 ? ' ' : '\n');
+        }
+    }
+    return 0;
+}
