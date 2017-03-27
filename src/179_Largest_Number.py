c27b3f6daf6627644606791376ba9e7aa9467e4f
ChuanLeiGuo <chuanleiguo@gmail.com>
2017-03-27 21:37:50 +0800

    deleted unnecessary files

--- src/179_Largest_Number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    # @param {integer[]} nums
-    # @return {string}
-    def largestNumber(self, nums):
-        arr = map(str, nums)
-        arr = sorted(arr, cmp=lambda x, y: int(y + x) - int(x + y))
-        res = "".join(arr)
-        return res if res[0] != '0' else '0'
--- src/319_Bulb_Switcher.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def bulbSwitch(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        from math import sqrt
-        return int(sqrt(n))
