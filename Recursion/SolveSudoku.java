d67fd5977beb94377f5c441894de195a126733c6
guo.chuanlei <chuanleiguo@gmail.com>
2018-04-04 15:12:30 +0800

    SolveSudoku

---
+++ Recursion/SolveSudoku.java
@@ -0,0 +1,77 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class SolveSudoku {
+
+    private static final int EMPTY_ENTRY = 0;
+
+    public static boolean solveSudoku(List<List<Integer>> partialAssignment) {
+        return solveSubSudoku(0, 0, partialAssignment);
+    }
+
+    private static boolean solveSubSudoku(int row, int col, List<List<Integer>> partialAssignment) {
+        if (row == partialAssignment.size()) {
+            row = 0;
+            if (++col == partialAssignment.get(row).size()) {
+                return true;
+            }
+        }
+
+        if (partialAssignment.get(row).get(col) != EMPTY_ENTRY) {
+            return solveSubSudoku(row + 1, col, partialAssignment);
+        }
+
+        for (int val = 1; val <= partialAssignment.size(); val++) {
+            if (validToAddVal(partialAssignment, row, col, val)) {
+                partialAssignment.get(row).set(col, val);
+                if (solveSubSudoku(row + 1, col, partialAssignment)) {
+                    return true;
+                }
+            }
+        }
+
+        partialAssignment.get(row).set(col, EMPTY_ENTRY);
+        return false;
+    }
+
+    private static boolean validToAddVal(List<List<Integer>> partialAssignment, int row, int col, int val) {
+        for (List<Integer> elem : partialAssignment) {
+            if (val == elem.get(col)) {
+                return false;
+            }
+        }
+
+        for (int k = 0; k < partialAssignment.size(); k++) {
+            if (val == partialAssignment.get(row).get(k)) {
+                return false;
+            }
+        }
+
+        int regionSize = (int) Math.sqrt(partialAssignment.size());
+        int R = row / regionSize, C = col / regionSize;
+        for (int a = 0; a < regionSize; a++) {
+            for (int b = 0; b < regionSize; b++) {
+                if (val == partialAssignment.get(regionSize * R + a).get(regionSize * C + b)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> A = Arrays.asList(Arrays.asList(0, 2, 6, 0, 0, 0, 8, 1, 0),
+                Arrays.asList(3, 0, 0, 7, 0, 8, 0, 0, 6), Arrays.asList(4, 0, 0, 0, 5, 0, 0, 0, 7),
+                Arrays.asList(0, 5, 0, 1, 0, 7, 0, 9, 0), Arrays.asList(0, 0, 3, 9, 0, 5, 1, 0, 0),
+                Arrays.asList(0, 4, 0, 3, 0, 2, 0, 5, 0), Arrays.asList(1, 0, 0, 0, 3, 0, 0, 0, 2),
+                Arrays.asList(5, 0, 0, 2, 0, 4, 0, 0, 9), Arrays.asList(0, 3, 8, 0, 0, 0, 4, 6, 0));
+        assert (solveSudoku(A));
+        List<List<Integer>> goldenA = Arrays.asList(Arrays.asList(7, 2, 6, 4, 9, 3, 8, 1, 5),
+                Arrays.asList(3, 1, 5, 7, 2, 8, 9, 4, 6), Arrays.asList(4, 8, 9, 6, 5, 1, 2, 3, 7),
+                Arrays.asList(8, 5, 2, 1, 4, 7, 6, 9, 3), Arrays.asList(6, 7, 3, 9, 8, 5, 1, 2, 4),
+                Arrays.asList(9, 4, 1, 3, 6, 2, 7, 5, 8), Arrays.asList(1, 9, 4, 8, 3, 6, 5, 7, 2),
+                Arrays.asList(5, 6, 7, 2, 1, 4, 3, 8, 9), Arrays.asList(2, 3, 8, 5, 7, 9, 4, 6, 1));
+        assert (A.equals(goldenA));
+    }
+}
