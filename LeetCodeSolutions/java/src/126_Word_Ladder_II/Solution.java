d2ad9875300a2883579409c44312b72b30a053b5
ChuanleiGuo <chuanleiguo@gmail.com>
2018-07-31 11:13:14 +0800

    126. Word Ladder II

---
+++ LeetCodeSolutions/java/src/126_Word_Ladder_II/Solution.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+class Solution {
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        Set<String> dict = new HashSet<>(wordList);
+        List<List<String>> res = new ArrayList<>();
+        Map<String, List<String>> nodeNeighbors = new HashMap<>();
+        Map<String, Integer> distance = new HashMap<>();
+        List<String> solution = new ArrayList<>();
+
+        if (!dict.contains(endWord)) {
+            return res;
+        }
+        dict.add(beginWord);
+        bfs(beginWord, endWord, dict, nodeNeighbors, distance);
+        dfs(beginWord, endWord, dict, nodeNeighbors, distance, solution, res);
+
+        return res;
+    }
+
+    private void bfs(String beginWord, String endWord, Set<String> dict, Map<String, List<String>> nodeNeighbors, Map<String, Integer> distance) {
+        for (String word : dict) {
+            nodeNeighbors.put(word, new ArrayList<>());
+        }
+        Deque<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        distance.put(beginWord, 0);
+
+        while (!queue.isEmpty()) {
+            int count = queue.size();
+            boolean foundEnd = false;
+            for (int i = 0; i < count; i++) {
+                String cur = queue.poll();
+                int curDistance = distance.get(cur);
+                List<String> neighbors = getNeighbors(cur, dict);
+
+                for (String neighbor: neighbors) {
+                    nodeNeighbors.get(cur).add(neighbor);
+                    if (!distance.containsKey(neighbor)) {
+                        distance.put(neighbor, curDistance + 1);
+                        if (endWord.equals(neighbor)) {
+                            foundEnd = true;
+                        } else {
+                            queue.offer(neighbor);
+                        }
+                    }
+                }
+
+                if (foundEnd) {
+                    break;
+                }
+            }
+        }
+    }
+
+    private List<String> getNeighbors(String node, Set<String> dict) {
+        List<String> res = new ArrayList<>();
+        char chs[] = node.toCharArray();
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            for (int i = 0; i < chs.length; i++) {
+                if (chs[i] == ch) {
+                    continue;
+                }
+                char oldChar = chs[i];
+                chs[i] = ch;
+                if (dict.contains(String.valueOf(chs))) {
+                    res.add(String.valueOf(chs));
+                }
+                chs[i] = oldChar;
+            }
+        }
+
+        return res;
+    }
+
+    private void dfs(String cur, String end, Set<String> dict, Map<String, List<String>> nodeNeighbors, Map<String, Integer> distance, List<String> solution, List<List<String>> res) {
+        solution.add(cur);
+        if (end.equals(cur)) {
+            res.add(new ArrayList<>(solution));
+        } else {
+            for (String next : nodeNeighbors.get(cur)) {            
+                if (distance.get(next) == distance.get(cur) + 1) {
+                     dfs(next, end, dict, nodeNeighbors, distance, solution, res);
+                }
+            }
+        }
+        solution.remove(solution.size() - 1);
+    } 
+}
