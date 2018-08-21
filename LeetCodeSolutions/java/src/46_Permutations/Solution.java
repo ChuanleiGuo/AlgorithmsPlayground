6259a31e5ff2b40bf7a4832b814913682e4c003b
ChuanleiGuo <chuanleiguo@gmail.com>
2018-08-21 09:04:08 +0800

    46. Permutations

---
+++ LeetCodeSolutions/java/src/46_Permutations/Solution.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        Arrays.fill(used, false);
+
+        generatePerms(nums, 0, new ArrayList<Integer>(), used, res);
+
+        return res;
+    }
+
+    private void generatePerms(int[] nums, int idx, List<Integer> p, boolean[] used, List<List<Integer>> res) {
+        if (idx == nums.length) {
+            res.add(new ArrayList<>(p));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!used[i]) {
+                p.add(nums[i]);
+                used[i] = true;
+                generatePerms(nums, idx + 1, p, used, res);
+                p.remove(p.size() - 1);
+                used[i] = false;
+            }
+        }
+        return;
+    }
+}
