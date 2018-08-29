c4b17632e96386bcfaec7e63b580b38ca25cea85
ChuanleiGuo <chuanleiguo@gmail.com>
2018-08-29 08:26:27 +0800

    120. Triangle

---
+++ LeetCodeSolutions/java/src/120_Triangle/Solution.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < i + 1; j++) {
+                int min = Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1));
+                triangle.get(i).set(j, triangle.get(i).get(j) + min);
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}
