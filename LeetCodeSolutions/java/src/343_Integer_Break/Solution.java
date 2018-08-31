6d2f8f299761f79ee13b50caef9085d884b8f8f0
ChuanleiGuo <chuanleiguo@gmail.com>
2018-08-31 23:05:38 +0800

    343

--- LeetCodeSolutions/java/src/343_Integer_Break/Solution.java
+++ LeetCodeSolutions/java/src/343_Integer_Break/Solution.java
@@ -2,11 +2,9 @@ class Solution {
     public int integerBreak(int n) {
         int[] dp = new int[n + 1];
         dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            int j = 1;
-            while ((j << 1) <= i) {
-                dp[i] = Math.max(dp[i], Math.max(dp[j], j) * Math.max(dp[i - j], i - j));
-                j++;
+        for (int i = 2; i <=n; i++) {
+            for (int j = 1; j <= i - 1; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
             }
         }
         return dp[n];
