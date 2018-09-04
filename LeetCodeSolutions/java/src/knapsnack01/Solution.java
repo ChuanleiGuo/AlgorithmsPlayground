1aa27f4c9be23510f32e867fd1375f3ab9aa7115
ChuanleiGuo <chuanleiguo@gmail.com>
2018-09-04 22:57:51 +0800

    0-1

---
+++ LeetCodeSolutions/java/src/knapsnack01/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+import java.util.List;
+
+public class Solution {
+
+    int[][] memo;
+
+    public int knapsnack01(List<Integer> weights, List<Integer> values, int c) {
+        int n = weights.size()
+        memo = new int[n][c + 1];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(memo[i], -1);
+        }
+        return bestValue(weights, values, n - 1, c);
+    }
+
+    private int bestValue(List<Integer> weights, List<Integer> values, int index, int c) {
+        if (index < 0 || c <= 0) {
+            return 0;
+        }
+
+        if (memo[index][c] != -1) {
+            return memo[index][c];
+        }
+        int res = bestValue(weights, values, index - 1, c);
+        if (c >= weights[index]) {
+            res = Math.max(res, values[index] + bestValue(weights, values, index - 1, c - w[index]));
+        }
+        memo[index][c] = res;
+        return res;
+    }
+}
