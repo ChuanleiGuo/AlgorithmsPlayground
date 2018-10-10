e3e1d866bee3eb89ee298f7ec0e78e8091daa219
ChuanleiGuo <chuanleiguo@gmail.com>
2018-10-10 09:23:07 +0800

    724. Find Pivot Index

---
+++ LeetCodeSolutions/java/src/724_Find_Pivot_Index/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int leftSum = 0;
+        int rightSum = sum;
+        for (int i = 0; i < nums.length; i++) {
+            rightSum -= nums[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
